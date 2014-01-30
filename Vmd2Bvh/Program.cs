a55f1d8bc378471baf86b263c3d4c7b07ab25c2b
Ichigo Mayo <esetomo@gmail.com>
2014-01-30 20:19:31 +0900

    modify Vmd2Bvh exception handler
    for bleak on debugger.

--- Vmd2Bvh/Program.cs
+++ Vmd2Bvh/Program.cs
@@ -40,77 +40,76 @@ namespace Vmd2Bvh
     {
         static void Main(string[] args)
         {
-            try
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            var options = new Options();
+            if (!CommandLine.Parser.Default.ParseArguments(args, options))
+                Environment.Exit(0);
+
+            if (options.Language != "english" && options.Language != "japanese")
             {
-                var options = new Options();
-                if (!CommandLine.Parser.Default.ParseArguments(args, options))
-                    Environment.Exit(0);
-
-                if (options.Language != "english" && options.Language != "japanese")
-                {
-                    Console.WriteLine("Language must be 'english' or 'japanese' (without the quote)");
-                    Environment.Exit(0);
-                }
-
-                if (options.Scale < 0f)
-                {
-                    Console.WriteLine("The scaling factor must be a positive real number.");
-                    Environment.Exit(0);
-                }
-
-                if (options.Files.Count < 2)
-                {
-                    Console.WriteLine(options.GetUsage());
-                    Environment.Exit(0);
-                }
-
-                Console.WriteLine("Loading PMD file {0}...", options.Files[0]);
-                var pmdModel = PmdParser.Parse(options.Files[0]);
-
-                Console.WriteLine("Loading VMD file...");
-                var vmdAnimation = VmdParser.Parse(options.Files[1]);
-
-                int firstFrame = vmdAnimation.GetFirstFrame();
-                int lastFrame = vmdAnimation.GetLastFrame();
-
-                var poseAnimation = vmdAnimation.GetIkLessPoseAnimation(pmdModel, (frame) =>
-                {
-                    Console.WriteLine("Frame #{0} out of {1}", frame, lastFrame - firstFrame + 1);
-                    return true;
-                });
-
-                var exportedContent = BvhExporter.Export(pmdModel.GetRestArmature(),
-                                                         poseAnimation,
-                                                         options.Language,
-                                                         30,
-                                                         options.Scale);
-
-                string bvhFileName;
-
-                if (options.Files.Count < 3)
-                {
-                    var modelName = Path.GetFileNameWithoutExtension(options.Files[0]);
-                    var animationPath = Path.GetFullPath(options.Files[1]);
-                    var animationName = Path.GetFileNameWithoutExtension(animationPath);
-                    var animationDir = Path.GetDirectoryName(animationPath);
-                    bvhFileName = Path.Combine(animationDir, modelName + "-" + animationName + ".bvh");
-                }
-                else
-                {
-                    bvhFileName = Path.GetFullPath(options.Files[2]);
-                }
-
-                Console.WriteLine("Saving BVH data to {0}", bvhFileName);
-
-                using (StreamWriter writer = new StreamWriter(bvhFileName))
-                {
-                    writer.Write(exportedContent);
-                }
+                Console.WriteLine("Language must be 'english' or 'japanese' (without the quote)");
+                Environment.Exit(0);
             }
-            catch(Exception e)
+
+            if (options.Scale < 0f)
             {
-                Console.WriteLine(e);
+                Console.WriteLine("The scaling factor must be a positive real number.");
+                Environment.Exit(0);
             }
+
+            if (options.Files.Count < 2)
+            {
+                Console.WriteLine(options.GetUsage());
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Loading PMD file {0}...", options.Files[0]);
+            var pmdModel = PmdParser.Parse(options.Files[0]);
+
+            Console.WriteLine("Loading VMD file...");
+            var vmdAnimation = VmdParser.Parse(options.Files[1]);
+
+            int firstFrame = vmdAnimation.GetFirstFrame();
+            int lastFrame = vmdAnimation.GetLastFrame();
+
+            var poseAnimation = vmdAnimation.GetIkLessPoseAnimation(pmdModel, (frame) =>
+            {
+                Console.WriteLine("Frame #{0} out of {1}", frame, lastFrame - firstFrame + 1);
+                return true;
+            });
+
+            var exportedContent = BvhExporter.Export(pmdModel.GetRestArmature(),
+                                                     poseAnimation,
+                                                     options.Language,
+                                                     30,
+                                                     options.Scale);
+
+            string bvhFileName;
+
+            if (options.Files.Count < 3)
+            {
+                var modelName = Path.GetFileNameWithoutExtension(options.Files[0]);
+                var animationPath = Path.GetFullPath(options.Files[1]);
+                var animationName = Path.GetFileNameWithoutExtension(animationPath);
+                var animationDir = Path.GetDirectoryName(animationPath);
+                bvhFileName = Path.Combine(animationDir, modelName + "-" + animationName + ".bvh");
+            }
+            else
+            {
+                bvhFileName = Path.GetFullPath(options.Files[2]);
+            }
+
+            Console.WriteLine("Saving BVH data to {0}", bvhFileName);
+
+            using (StreamWriter writer = new StreamWriter(bvhFileName))
+            {
+                writer.Write(exportedContent);
+            }
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine(e.ExceptionObject);            
         }
     }
 }

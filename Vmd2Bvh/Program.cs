5e5e88c0f54453c4ffadc96c72c1f579319c563b
Ichigo Mayo <esetomo@gmail.com>
2014-01-30 12:46:32 +0900

    added C# projects

---
+++ Vmd2Bvh/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// C# Version of vmd2bvh.py
+namespace Vmd2Bvh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
